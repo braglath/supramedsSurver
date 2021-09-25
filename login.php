bdf3e17ff3d88f7232d3224f7bf948f226b41b57
braglath <braglath44@gmail.com>
2021-09-26 02:40:14 +0530

    add data

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php 
 	require_once("connect.php");
-    $phone_number = $_POST['phone_number'];
+    $phone_number = $_POST['phone_number']?? "";
 
     $sql = "SELECT * FROM users WHERE phone_number = '".$phone_number."'";
     
     $result = mysqli_query($conn,$sql);
     $count = mysqli_num_rows($result);
 
-    if($count == 1){
+    if(mysqli_num_rows($result) > 0){
         echo json_encode("Success");
     }else{
         echo json_encode("Error");
