ac7a56b533d921ac9ec7f54655fbbf950c39f7c1
braglath <braglath44@gmail.com>
2021-09-26 16:13:26 +0530

    add.php

--- signup.php
+++ signup.php
@@ -5,9 +5,10 @@
 	$email = $_POST['email']?? "";
 	$phoneNumber = $_POST['phone_number']?? "";
 	$password = $_POST['password']?? "";
+	$address = $_POST['address']?? "";
     
 
-	$query = "SELECT * FROM users WHERE name ='".$name."' AND email = '".$email."' AND phone_number = '".$phoneNumber."' AND password = '".$password."'";
+	$query = "SELECT * FROM users WHERE name ='".$name."' AND email = '".$email."' AND phone_number = '".$phoneNumber."' AND password = '".$password."' AND address = '".$address."'";
 	$res = mysqli_query($conn,$query);
 	$data = mysqli_num_rows($res);
 
@@ -16,7 +17,7 @@
 		echo json_encode("Error");
 	} else {
 		//  create account
-		$query = "INSERT INTO users(name,email,phone_number,password)VALUES('".$name."','".$email."','".$phoneNumber."','".$password."')";
+		$query = "INSERT INTO users(name,email,phone_number,password,address)VALUES('".$name."','".$email."','".$phoneNumber."','".$password."','".$address."' )";
 		$res = mysqli_query($conn,$query);
 
 		if($res){
